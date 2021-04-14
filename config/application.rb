f2407a78b08c3d16a42c7025fb368f776eb15832
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-04-14 17:33:30 +0900

    テーブルの追加

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module JwtTest
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -18,5 +19,7 @@ module JwtTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.debug_exception_response_format = :api
   end
 end
